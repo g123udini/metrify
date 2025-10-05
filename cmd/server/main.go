8ece6721097a887e53e4f2aa6871fffc319fce49
Kirill Sokolov <k.sokolov@seowork.ru>
2025-10-06 01:26:08 +0700

    done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"metrify/internal/handler"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/update/", handler.Middleware(http.HandlerFunc(handler.Update)))
+
+	err := http.ListenAndServe(":8080", mux)
+
+	if err != nil {
+		panic(err)
+	}
+}
