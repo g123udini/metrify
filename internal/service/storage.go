02ab6618762c21639804a7a94d9ef50e786c8819
Kirill Sokolov <k.sokolov@seowork.ru>
2025-10-14 11:58:58 +0700

    refactored storage

--- internal/service/storage.go
+++ internal/service/storage.go
@@ -1,10 +1,14 @@
 package service
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"sync"
+)
 
 type MemStorage struct {
 	gauges   map[string]float64
 	counters map[string]int64
+	mu       sync.RWMutex
 }
 
 func NewMemStorage() *MemStorage {
@@ -15,29 +19,41 @@ func NewMemStorage() *MemStorage {
 }
 
 type Storage interface {
-	GetCounter(key string) int64
-	GetGauge(key string) float64
+	GetCounter(key string) (int64, bool)
+	GetGauge(key string) (float64, bool)
 	UpdateGauge(name string, value float64)
 	UpdateCounter(name string, delta int64)
 }
 
 func (ms *MemStorage) GetCounter(key string) (int64, bool) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
 	val, ok := ms.counters[key]
 
 	return val, ok
 }
 
 func (ms *MemStorage) GetGauge(key string) (float64, bool) {
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
+
 	val, ok := ms.gauges[key]
 
 	return val, ok
 }
 
 func (ms *MemStorage) UpdateGauge(name string, value float64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
 	ms.gauges[name] = value
 }
 
 func (ms *MemStorage) UpdateCounter(name string, delta int64) {
+	ms.mu.Lock()
+	defer ms.mu.Unlock()
+
 	ms.counters[name] += delta
 }
 
@@ -46,6 +62,9 @@ func (ms *MemStorage) MarshalJSON() ([]byte, error) {
 		Gauges   map[string]float64 `json:"gauges"`
 		Counters map[string]int64   `json:"counters"`
 	}
+
+	ms.mu.RLock()
+	defer ms.mu.RUnlock()
 
 	result := dto{
 		Gauges:   ms.gauges,
