a60863e0d973a608370960c4ef2351eee0f81b12
Kirill Sokolov <k.sokolov@seowork.ru>
2025-11-18 18:52:04 +0700

    done

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/golang-migrate/migrate/v4 v4.19.0
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/stretchr/testify v1.11.1
 	go.uber.org/zap v1.27.0
--- internal/service/retry.go
+++ internal/service/retry.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"errors"
 	"fmt"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 	"time"
 )
 
@@ -15,6 +18,30 @@ func Retry[T any](attempts int, base time.Duration, step time.Duration, fn func(
 		res, err = fn()
 		if err == nil {
 			return res, nil
+		}
+
+		delay := base + step*time.Duration(i)
+
+		time.Sleep(delay)
+	}
+
+	return res, fmt.Errorf("after %d attempts, last error: %w", attempts, err)
+}
+
+func RetryDB[T any](attempts int, base time.Duration, step time.Duration, fn func() (T, error)) (T, error) {
+	var (
+		res T
+		err error
+	)
+
+	for i := 1; i <= attempts; i++ {
+		res, err = fn()
+		var pgErr *pgconn.PgError
+
+		if errors.As(err, &pgErr) {
+			if pgErr.SQLState() != pgerrcode.ConnectionFailure || pgErr.SQLState() != pgerrcode.TooManyConnections {
+				return res, nil
+			}
 		}
 
 		delay := base + step*time.Duration(i)
